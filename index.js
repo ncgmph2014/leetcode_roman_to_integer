315e9c95861d318aa33054ad9ba6bc120cc52f1a
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-18 15:19:27 -0500

    tried something, it does not work

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ var romanToInt = function(s) {
   //I comes before V or X
   //X  comes before L or C
   //C comes before D or M
+  //this will not work for the last case
+  let firstI = 0;
+  let firstV = 0;
+  let firstX = 0;
+  let firstL = 0;
+  let firstC = 0;
+  let firstD = 0;
+  let firstM = 0;
+  for (let i = s.length - 1; s >= 0; i--) {}
 };
 
 romanToInt("III");
@@ -14,3 +23,12 @@ romanToInt("IV");
 romanToInt("IX");
 romanToInt("LVIII");
 romanToInt("MCMXCIV");
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
