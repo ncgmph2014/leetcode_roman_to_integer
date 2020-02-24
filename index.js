537c68dfccfc476f7180eb69b15acf1acd9ea859
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-24 14:55:21 -0500

    finished all the 4, 9, 40, 90, 400, 900 cases

--- index.js
+++ index.js
@@ -30,10 +30,63 @@ var romanToInt = function(s) {
   let lastD = s.lastIndexOf("D");
   let lastM = s.lastIndexOf("M");
 
-  console.log(firstX - firstI === 1);
-  console.log(firstX - lastI === 1);
-  console.log(lastX - firstI === 1);
-  console.log(lastX - lastI === 1);
+  //All the weird cases
+  //for 4 and 9
+  if (firstI !== -1) {
+    if (
+      firstV - firstI === 1 ||
+      firstV - lastI === 1 ||
+      lastV - firstI === 1 ||
+      lastV - lastI === 1
+    ) {
+      sum += 4;
+    } else if (
+      firstX - firstI === 1 ||
+      firstX - lastI === 1 ||
+      lastX - firstI === 1 ||
+      lastX - lastI === 1
+    ) {
+      sum += 9;
+    }
+  }
+
+  //for 40 and 90
+  if (firstX !== -1) {
+    if (
+      firstL - firstX === 1 ||
+      firstL - lastX === 1 ||
+      lastL - firstX === 1 ||
+      lastL - lastX === 1
+    ) {
+      sum += 40;
+    } else if (
+      firstC - firstX === 1 ||
+      firstC - lastX === 1 ||
+      lastC - firstX === 1 ||
+      lastC - lastX === 1
+    ) {
+      sum += 90;
+    }
+  }
+
+  if (firstC !== -1) {
+    if (
+      firstD - firstC === 1 ||
+      firstD - lastC === 1 ||
+      lastD - lastC === 1 ||
+      lastD - firstC === 1
+    ) {
+      sum += 400;
+    } else if (
+      firstM - firstC === 1 ||
+      firstM - lastC === 1 ||
+      lastM - lastC === 1 ||
+      lastM - firstC === 1
+    ) {
+      sum += 900;
+    }
+  }
+  //for 400 and 900
 
   //creating condition where I can add everything easily
   //if no I's come before X or V
@@ -42,13 +95,16 @@ var romanToInt = function(s) {
 };
 
 // console.log(romanToInt("III") + " should be 3");
-// console.log(romanToInt("IV") + " should be 4");
-// console.log(romanToInt("IX") + " should be 9");
-console.log(romanToInt("LVIII") + " should be 58");
+console.log(romanToInt("IV") + " should be 4");
+console.log(romanToInt("IX") + " should be 9");
+// console.log(romanToInt("LVIII") + " should be 58");
 console.log(romanToInt("MCMXCIV") + " should be 1994");
 console.log(romanToInt("CD") + " should be 400");
-console.log(romanToInt("XIX") + "should be 19");
-// console.log(romanToInt("XV") + " should be 15");
+console.log(romanToInt("XIX") + " should be 19");
+// console.log(romanToInt("XLIV") + " should be 44");
+console.log(romanToInt("CDXLIV") + " should be 444");
+console.log(romanToInt("XV") + " should be 15");
+console.log(romanToInt("M") + " should be 1000");
 
 // Symbol       Value
 // I             1
